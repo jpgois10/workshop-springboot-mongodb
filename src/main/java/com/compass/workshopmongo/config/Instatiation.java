f3d9fb898fb1090f7f5a26ecfacc886f72d09aad
Joao Paulo Barbosa <jpbgois10@gmail.com>
2024-12-27 17:34:58 -0300

    Referenciando os posts do usuário

--- src/main/java/com/compass/workshopmongo/config/Instatiation.java
+++ src/main/java/com/compass/workshopmongo/config/Instatiation.java
@@ -42,5 +42,8 @@ public class Instatiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria) );
 
         postRepository.saveAll(List.of(post1, post2));
+
+        maria.getPosts().addAll(List.of(post1, post2));
+        userRepository.save(maria);
     }
 }
