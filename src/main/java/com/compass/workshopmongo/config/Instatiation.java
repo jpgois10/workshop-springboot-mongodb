7db5c5d8586da92aef1b107a623c288128243f65
Joao Paulo Barbosa <jpbgois10@gmail.com>
2024-12-27 17:16:05 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/compass/workshopmongo/config/Instatiation.java
+++ src/main/java/com/compass/workshopmongo/config/Instatiation.java
@@ -2,6 +2,7 @@ package com.compass.workshopmongo.config;
 
 import com.compass.workshopmongo.domain.Post;
 import com.compass.workshopmongo.domain.User;
+import com.compass.workshopmongo.dto.AuthorDTO;
 import com.compass.workshopmongo.repository.PostRepository;
 import com.compass.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,11 +36,11 @@ public class Instatiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria );
-        Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei feliz hoje!", maria );
-
-
         userRepository.saveAll(List.of(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria) );
+
         postRepository.saveAll(List.of(post1, post2));
     }
 }
