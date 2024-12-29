81a5fa47ea979fc932c835f0ba68e3b900344f9a
Joao Paulo Barbosa <jpbgois10@gmail.com>
2024-12-29 17:25:42 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/compass/workshopmongo/config/Instatiation.java
+++ src/main/java/com/compass/workshopmongo/config/Instatiation.java
@@ -3,6 +3,7 @@ package com.compass.workshopmongo.config;
 import com.compass.workshopmongo.domain.Post;
 import com.compass.workshopmongo.domain.User;
 import com.compass.workshopmongo.dto.AuthorDTO;
+import com.compass.workshopmongo.dto.CommentDTO;
 import com.compass.workshopmongo.repository.PostRepository;
 import com.compass.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,13 @@ public class Instatiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria) );
+
+        CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO comment2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(List.of(comment1, comment2));
+        post2.getComments().addAll(List.of(comment3));
 
         postRepository.saveAll(List.of(post1, post2));
 
